090e0d929b9394927c80dcc4c29c6286faf2d7b0
rizdaprasetya <rizda.prasetya@midtrans.com>
2020-08-19 13:11:30 +0700

    move wc order query hook to proper scope when handling debit finish page
    - fix uncaught error when order not found
    - side effect: merchant will need to rename finish page from `payment-finish` to `midtrans-payment-finish

--- midtrans-gateway.php
+++ midtrans-gateway.php
@@ -114,21 +114,3 @@ function plugin_action_links($links){
   );
   return array_merge($plugin_links, $links);
 }
-
-/**
- * Handle a custom '_mt_payment_transaction_id' query var to get orders with the '_mt_payment_transaction_id' meta.
- * @param array $query - Args for WP_Query.
- * @param array $query_vars - Query vars from WC_Order_Query.
- * @return array modified $query
- */
-function handle_custom_query_var( $query, $query_vars ) {
-	if ( ! empty( $query_vars['_mt_payment_transaction_id'] ) ) {
-		$query['meta_query'][] = array(
-			'key' => '_mt_payment_transaction_id',
-			'value' => esc_attr( $query_vars['_mt_payment_transaction_id'] ),
-		);
-	}
-
-	return $query;
-}
-add_filter( 'woocommerce_order_data_store_cpt_get_orders_query', 'handle_custom_query_var', 10, 2 );
