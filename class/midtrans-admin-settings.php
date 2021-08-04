580d62f0b3c340dd1608524837a1dc7f567766c1
rizdaprasetya <rizdaprasetya@users.noreply.github.com>
2021-08-04 16:06:14 +0700

    change immediate-reduce-stock default config val to yes
    - to prepare removal of this config, it should be the default behaviour

--- class/midtrans-admin-settings.php
+++ class/midtrans-admin-settings.php
@@ -149,7 +149,7 @@ return apply_filters(
             'type'          => 'checkbox',
             'label'         => 'Immediately reduce item stock on Midtrans payment pop-up?',
             'description'   => __( 'By default, item stock only reduced if payment status on Midtrans reach pending/success (customer choose payment channel and click pay on payment pop-up). Enable this if you want to immediately reduce item stock when payment pop-up generated/displayed.', 'midtrans-woocommerce' ),
-            'default'       => 'no'
+            'default'       => 'yes'
         ),
         // @Note: only main plugin class config will be applied on notif handler, sub plugin class config will not affect it, check gateway-notif-handler.php class to fix
         'ignore_pending_status'         => array(
